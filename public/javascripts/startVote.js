4751f6eb338f7504b5f9596a96ef15fe42927230
Sonali <sojo81@yahoo.com>
2012-06-08 13:12:39 -0400

    display votes to the moderator

--- public/javascripts/startVote.js
+++ public/javascripts/startVote.js
@@ -1,2 +1,27 @@
 var socket = io.connect('http://10.101.95.227:3001');
 socket.emit('voteStarted', {});
+
+socket.on('gameEnded', function (data) {
+	var results = document.getElementById("results");
+	
+	var message = document.createElement("div");
+	message.setAttribute("id", "finalVotes"); 
+	message.innerHTML = "The votes are:"
+	results.appendChild(message);
+	
+	var resultsList = document.createElement("ul");
+	results.appendChild(resultsList); 
+	
+	var usersVotes = data['usersVotes'];
+	
+	for (var key in usersVotes) {
+		var vote = document.createElement("li");
+		vote.innerHTML = usersVotes[key];
+		resultsList.appendChild(vote);
+	}
+	
+	var voteInProgress = document.getElementById("voteInProgress");
+	voteInProgress.style.display = "none";
+	
+});
+
